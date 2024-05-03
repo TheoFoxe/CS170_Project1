2a12a534b124eec106cc9f8733da061006a60012
TheoFoxe <chantal.k27th@gmail.com>
2024-05-02 18:49:28 -0700

    Starting to work on StateRep class

---
+++ StateRep.h
@@ -0,0 +1,26 @@
+#ifndef STATEREP_H
+#define STATEREP_H
+
+#include <vector>
+#include <string>
+
+/*
+All info relevant to state
+
+- state rep
+- total cost
+- heuristic cost
+- 
+*/
+
+struct Node 
+
+class StateRep {
+    private:
+
+    public: 
+
+
+};
+
+#endif
