ad1a6d55c0084139569e26167d0776c6e44ab929
Jay <72745302+lolerj@users.noreply.github.com>
2024-05-05 00:03:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,63 @@
 #include <iostream>
 #include "Problem.h"
+#include "StateRep.h"
 #include <string>
 #include <vector>
 
-void inputPuzzle()
+void inputPuzzle(std::vector<std::vector<int>> userDefinedPuzzle)
 {
+    std::string order_string[3] = {"first", "second", "third"};
     std::cout << "Enter your puzzle, use a zero to represent the blank\n";
+    for (int i = 0; i < 3; i++){//set up an user defined puzzle
+        std::cout << "Enter the " << order_string[i] << " row, use space or tabs between numbers: ";
+        for (int j = 0; j < 3; j++)
+        {
+            std::cin >> userDefinedPuzzle[i][j];
+        }
+    }
 }
 
 int main()
-{
-    //building a default puzzle
-    //1 2 3
-    //4 5 6
-    //7 8 0
-    std::vector<int>initialState = {{0,0,0,0,0,0,0,0}};
-    std::vector<int>GoalState = {1,2,3,4,5,6,7,8,0}; 
-    Operator up("Up", 2);
-    Operator left("Left", 4);
-    Operator right("Right", 6);
-    Operator down("Down", 8);
+{   
+    Operator up("Up", -3);
+    Operator left("Left", -1);
+    Operator right("Right", 1);
+    Operator down("Down", 3);
     std::vector<Operator> operators = {up, down, left, right};
-    
-    int choice;
+    std::vector<std::vector<int>> userDefinedPuzzle;
+    std::vector<std::vector<int>> defaultPuzzle = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0}
+    };
+
+    int PuzzleChoice;
     std::cout << "Welcome to XXX (change this to your student ID) 8 puzzle solver.\n";
     std::cout << "Type '1' to use a default puzzle, or '2' to enter your own puzzle.\n";
-    Problem puzzle(initialState, GoalState, operators);
-    if (choice == 2) {
-        //inputpuzzle to get userinput
-    } else {
-        
-        // Set up a default puzzle
+    if (PuzzleChoice == 2) {//set up an user defined puzzle
+        inputPuzzle(userDefinedPuzzle);
+    } 
+
+    int AlgorithmChoice;
+    std::cout << "Enter your choice of algorithm (1/2/3)\n";
+    std::cout << "1. Uniform Cost Search\n";
+    std::cout << "2. A* with the Misplaced Tile heuristic.\n";
+    std::cout << "3. A* with the Euclidean distance heuristic.\n";
+    std::cin >> AlgorithmChoice;
+
+    if(AlgorithmChoice == 1){
+        std::cout << "executing Uniform Cost Search\n";
     }
+    else if(AlgorithmChoice == 2){
+        std::cout << "executing A* with the Misplaced Tile heuristic\n";
+    }
+    else if(AlgorithmChoice == 3){
+        std::cout << "executing A* with the Euclidean distance heuristic\n";
+    }
+    else{
+        std::cout << "Invalid algorithm choice.\n";
+        return 1;
+    }
+
     return 0;
 }
