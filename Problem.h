f17a89767b6505b00d18c1e9abbc326e92f62e08
ashley-bautista <bautistaashley907@gmail.com>
2024-05-05 21:01:32 -0700

    added new staterep files replacing 2d arrays w 2d vectors

--- Problem.h
+++ Problem.h
@@ -7,17 +7,17 @@
 
 class Problem {
 private:
-    std::vector<std::vector<int>> initialState;
-    std::vector<std::vector<int>> goalState;
+    std::vector<std::vector<int> > initialState;
+    std::vector<std::vector<int> > goalState;
     std::vector<std::string> operators;
-    std::pair<int, int> getBlankPos(std::vector<std::vector<int>>& state);
+    std::pair<int, int> getBlankPos(std::vector<std::vector<int> >& state);
     std::pair<int, int> getNewPos(std::string opName, std::pair<int, int> blankPos);
 
 public:
-    Problem(std::vector<std::vector<int>>& init, std::vector<std::vector<int>>&goal, std::vector<std::string> ops);
-    std::vector<std::vector<int>> getInitalState();
-    std::vector<std::vector<int>> getGoalState();
+    Problem(std::vector<std::vector<int> >& init, std::vector<std::vector<int> >&goal, std::vector<std::string> ops);
+    std::vector<std::vector<int> > getInitalState();
+    std::vector<std::vector<int> > getGoalState();
     std::vector<std::string> getOperators();
-    std::vector<std::vector<int>> applyOperation(std::vector<std::vector<int>>& state, std::string opName);
+    std::vector<std::vector<int> > applyOperation(std::vector<std::vector<int> >& state, std::string opName);
 };
 #endif
