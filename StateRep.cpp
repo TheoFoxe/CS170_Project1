c07e30ad185ddf62b6df969c49592a350c344d09
TheoFoxe <chantal.k27th@gmail.com>
2024-05-06 01:49:29 -0700

    fixing to 2d arrays to 2d vects

--- StateRep.cpp
+++ StateRep.cpp
@@ -4,32 +4,40 @@ using namespace std;
 
 // constructors ------------------
 
-StateRep::StateRep(int state[puzzleDim][puzzleDim]) {
-    setState(state);
+StateRep::StateRep() {
+    this->state = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    this->totalCost = 0;
+    this->heuristicCost = 0;
 }
 
-StateRep::StateRep(int state[puzzleDim][puzzleDim], double totalCost, double heuristicCost) {
-    setState(state);
+StateRep::StateRep(vector<vector<int>> state) {
+    this->state = state;
+    this->totalCost = 0;
+    this->heuristicCost = 0;
+}
+
+StateRep::StateRep(vector<vector<int>> state, double totalCost, double heuristicCost) {
+    this->state = state;
     this->totalCost = totalCost;
     this->heuristicCost = heuristicCost;
 }
 
 // state ----------------------
 
-void StateRep::setState(int state[puzzleDim][puzzleDim]) {
-    for (unsigned int i = 0; i < 3; ++i) {
-        for (unsigned int j = 0; j < 3; ++j) {
-            this->state[i][j] = state[i][j];
-        }
-    }
+void StateRep::setState(vector<vector<int>>) {
+    this->state = state;
+}
+
+const vector<vector<int>> & getState() const {
+    return this->state;
 }
 
 void StateRep::printState() const {
-    for (unsigned int i = 0; i < puzzleDim; ++i) {
-        for (unsigned int j = 0; j < puzzleDim; ++j) {
+    for (unsigned int i = 0; i < state.size(); ++i) {
+        for (unsigned int j = 0; j < state[i].size(); ++j) {
             cout << state[i][j] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
 }
 
