dcce63122297901e4d11ac0b4f652d87b1d02b02
ashley-bautista <bautistaashley907@gmail.com>
2024-05-06 22:00:52 -0700

    omit problem.cpp, astareuclideansolver.cpp, changes to main.cpp + changes to problem.h + changes to astareuclideansolver.h

--- AstarEuclideanSolver.h
+++ AstarEuclideanSolver.h
@@ -1,18 +1,139 @@
 #ifndef ASTAR_EUCLIDEAN_SOLVER_H
 #define ASTAR_EUCLIDEAN_SOLVER_H
 
-#include <vector>
+#include <set>
+#include <string>
+#include <queue>
+#include <cmath>
+#include "Problem.h"
 
-struct Node
-{
-    std::vector<std::vector<int> > state; // Here
-    double g;                            // Cost from start node to current node
-    double h;                            // Heuristic value (Euclidean distance)
+//define movement directions
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+
+//its to keep track of max queue size + #expanded nodes
+int XXX = 1; //1 bc initial state alr in queue
+int YYY;
+int ZZZ;
+class EuclideanDistanceSolver {
+public:
+    EuclideanDistanceSolver(Problem& goal) : goal(goal) {};
+
+    //calc euclidean distance between goal/curr
+    virtual double heuristicFunction(Problem &current) {
+        double distance = 0.0;
+        for (int i = 0; i < PUZZLE_SIZE; ++i) {
+            for (int j = 0; j < PUZZLE_SIZE; ++j) {
+                int value = current.getIndexOfRowCol(i, j);
+                if (value != 0) {
+                    // Find position of value/tile in the goal state
+                    for (size_t k = 0; k < PUZZLE_SIZE; ++k) {
+                        for (size_t m = 0; m < PUZZLE_SIZE; ++m) {
+                            if (value == goal.getIndexOfRowCol(k, m)) {
+                                int goal_row = k;
+                                int goal_col = m;
+                                //calc + add euclidean distance to total distance
+                                distance += sqrt(pow(i - goal_row, 2) + pow(j - goal_col, 2));
+                                k = PUZZLE_SIZE;
+                                m = PUZZLE_SIZE;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return distance;
+    };
+
+    bool solveProblem(Problem& current) {
+        cout << "Expanding state" << endl;
+        cout << current << endl;
+
+        //store puzzles based on total cost (f(n) = g(n) + h(n))
+        priority_queue<Problem> mq;
+        //set to keep track of visited states
+        set<string> vis;
+
+        //save initial puzzle into priority_queue
+        mq.push(current);
+       //mark initial state as visited
+        vis.insert(current.getHashString());
+
+        while (!mq.empty()) {
+            //get puzzle w lowest total cost
+            Problem top = mq.top();
+            mq.pop();
+
+            //check if current state is goal
+            if (top == goal) {
+                ZZZ = top.getG();//store depth of goal node
+
+                cout << "We solved the puzzle!" << endl;
+                cout << "Use the move list: " << top.op << endl;
+
+                return true;
+            }
+
+            //iterate through possible moves 
+            for (int i = 0; i < 4; i++) {
+                int nx = top.getZeroRow() + dx[i];
+                int ny = top.getZeroCol() + dy[i];
+
+                //check if move is within bounds
+                if (nx < 0 || nx > PUZZLE_SIZE - 1 || ny < 0 || ny > PUZZLE_SIZE - 1)
+                    continue;
+                
+                //get val of tile @ new position
+                int value = top.getIndexOfRowCol(nx, ny);
+                int zero_row = top.getZeroRow();
+                int zero_col = top.getZeroCol();
+
+                //create new puzzle by moving 0 tile to new position
+                Problem temp = top;
+                temp.setIndexOfRowCol(nx, ny, 0);
+                temp.setIndexOfRowCol(zero_row, zero_col, value);
+                temp.setG(top.getG() + 1); //increment depth
+                temp.setH(heuristicFunction(temp)); //calc heuristic val
+
+                //check if new state has been visited before
+                if (vis.find(temp.getHashString()) == vis.end()) {
+                    cout << "The best state to expand with g(n)=" << temp.getG();
+                    cout << " and h(n)=" << temp.getH() << " is..." << endl;
+                    cout << temp << endl;
+
+                    //update move list
+                    switch (i)
+                    {
+                    case 0:
+                        temp.op += "D";
+                        break;
+
+                    case 1:
+                        temp.op += "U";
+                        break;
+
+                    case 2:
+                        temp.op += "R";
+                        break;
+
+                    case 3:
+                        temp.op += "L";
+                        break;
+                    }
+
+                    mq.push(temp); //add new state to priority queue
+                    XXX++; //increment # of expanded nodes
+                    YYY = max(YYY, (int)mq.size()); //update max queue size
+                    vis.insert(temp.getHashString()); //mark new state visited
+                }
+            }
+        }
+
+        return false;
+    };
+
+protected:
+    Problem goal;
 };
-
-// A* Search Algorithm with the Euclidean Distance heuristic.
-std::vector<std::vector<int> > aStarSearch(const std::vector<std::vector<int> > &initial, const std::vector<std::vector<int> > &goal); // Here
-
-const std::__1::vector<std::__1::vector<int> > &NewFunction();
 
 #endif // ASTAR_EUCLIDEAN_SOLVER_H
--- main.cpp
+++ main.cpp
@@ -1,51 +1,64 @@
 #include <iostream>
-#include "Problem.h"
-#include "StateRep.h"
 #include <string>
 #include <vector>
+
+#include "Problem.h"
+#include "AstarEuclideanSolver.h"
+using namespace std;
 
 void inputPuzzle(std::vector<std::vector<int>>& userDefinedPuzzle)
 {
     std::string order_string[3] = {"first", "second", "third"};
     std::cout << "Enter your puzzle, use a zero to represent the blank\n";
-    for (int i = 0; i < 3; i++){//set up an user defined puzzle
+    for (int i = 0; i < PUZZLE_SIZE; i++){//set up an user defined puzzle
         std::cout << "Enter the " << order_string[i] << " row, use space or tabs between numbers: ";
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < PUZZLE_SIZE; j++)
         {
             std::cin >> userDefinedPuzzle[i][j];
         }
     }
-    std::cout << "This is your current pazzle\n";
-    for (int i = 0; i < 3; i++){//set up an user defined puzzle;
-        std::cout << "\n";
-        for (int j = 0; j < 3; j++)
-        {
-            std::cout << userDefinedPuzzle[i][j];
-        }
-    }
+    cout << endl;
 }
 
 int main()
 {   
-    std::vector<std::string> operators = {"up", "down", "left", "right"};
-    std::vector<std::vector<int>> userDefinedPuzzle = {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    };
-    std::vector<std::vector<int>> defaultPuzzle = {
+    std::vector<std::vector<int>> goalPuzzleContent = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 0}
     };
+    Problem goalPuzzle(goalPuzzleContent);
 
-    int PuzzleChoice;
+    std::vector<std::vector<int>> defaultPuzzleContent = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 0, 8}
+    };
+
+    std::vector<std::vector<int>> userDefinedPuzzleContent = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+    Problem currentPuzzle;
+
+    size_t PuzzleChoice;
+
     std::cout << "Welcome to XXX (change this to your student ID) 8 puzzle solver.\n";
     std::cout << "Type '1' to use a default puzzle, or '2' to enter your own puzzle.\n";
     std::cin >> PuzzleChoice;
-    if (PuzzleChoice == 2) {//set up an user defined puzzle
-        inputPuzzle(userDefinedPuzzle);
-    } 
+    if(PuzzleChoice == 1){//display default puzzle
+        currentPuzzle = Problem(defaultPuzzleContent);
+    }
+    else if (PuzzleChoice == 2) {//set up an user defined puzzle and display
+        inputPuzzle(userDefinedPuzzleContent);
+        currentPuzzle = Problem(userDefinedPuzzleContent);
+    }
+    else {
+        cout << "Error choice" << endl;
+        return 0;
+    }
+    cout << currentPuzzle << endl;
 
     int AlgorithmChoice;
     std::cout << "Enter your choice of algorithm (1/2/3)\n";
@@ -54,18 +67,39 @@ int main()
     std::cout << "3. A* with the Euclidean distance heuristic.\n";
     std::cin >> AlgorithmChoice;
 
-    if(AlgorithmChoice == 1){
-        std::cout << "executing Uniform Cost Search\n";
-    }
-    else if(AlgorithmChoice == 2){
-        std::cout << "executing A* with the Misplaced Tile heuristic\n";
-    }
-    else if(AlgorithmChoice == 3){
+    bool is_solved = false;
+    // if(AlgorithmChoice == 1){
+    //     std::cout << "executing Uniform Cost Search\n";
+    //     UniformCostSolver solver(goalPuzzle);
+    //     is_solved = solver.solvePuzzle(currentPuzzle);
+    // }
+    // else if(AlgorithmChoice == 2){
+    //     std::cout << "executing A* with the Misplaced Tile heuristic\n";
+    //     MisplacedTileSolver solver(goalPuzzle);
+    //     is_solved = solver.solvePuzzle(currentPuzzle);
+    // }
+    // else if(AlgorithmChoice == 3){
+    //     std::cout << "executing A* with the Euclidean distance heuristic\n";
+    //     EuclideanDistanceSolver solver(goalPuzzle);
+    //     is_solved = solver.solvePuzzle(currentPuzzle);
+    // }
+    if(AlgorithmChoice == 3){
         std::cout << "executing A* with the Euclidean distance heuristic\n";
+        EuclideanDistanceSolver solver(goalPuzzle);
+        is_solved = solver.solveProblem(currentPuzzle);
     }
     else{
         std::cout << "Invalid algorithm choice.\n";
         return 1;
+    }
+
+    if (is_solved) {
+        cout << "To solve this problem the search algorithm expanded a total of " << XXX << " nodes." << endl;
+        cout << "The maximum number of nodes in the queue at any one time: " << YYY << "." << endl;
+        cout << "The depth of the goal node was  " << ZZZ << "." << endl;
+    }
+    else {
+        cout << "The puzzle is impossible to solve" << endl;
     }
 
     return 0;
