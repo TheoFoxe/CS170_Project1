31b2758237f662bc8667ce16f45fd0a94407b4d6
Chengzhe Liu <siinheon@gmail.com>
2024-05-06 22:50:48 -0700

    Misplaced solver

---
+++ Misplaced.h
@@ -0,0 +1,32 @@
+#ifndef ASTAR_MISPLACED_SOLVER_H
+#define ASTAR_MISPLACED_SOLVER_H
+
+#include <set>
+#include <string>
+#include <queue>
+#include <cmath>
+#include "Puzzle.h"
+#include "UniformCostSearch.h"
+
+
+
+// Calculate the heuristic cost using the Misplaced Tile heuristic
+class MisplacedTileSolver : public UniformCostSolver {
+public:
+    //set up goal puzzle
+    MisplacedTileSolver(Puzzle& goal) : UniformCostSolver(goal) {};
+    virtual double heuristicFunction(Puzzle& current) override {
+        double misplaced = 0;
+        for (size_t i = 0; i < PUZZLE_SIZE; ++i) {
+            for (size_t j = 0; j < PUZZLE_SIZE; ++j) {
+                if (current.getIndexOfRowCol(i, j) != 0 && current.getIndexOfRowCol(i, j) != goal.getIndexOfRowCol(i, j)) {
+                    misplaced++;
+                }
+            }
+        }
+        return misplaced;
+    };
+};
+
+
+#endif // ASTAR_MISPLACED_SOLVER_H
